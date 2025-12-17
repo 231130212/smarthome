b97239b5172b1e01d679445fb823ff71a8cc7605
chuseokk <167173348+231130212@users.noreply.github.com>
2025-12-17 21:50:39 +0700

    Update sensor.js

--- js/sensor.js
+++ js/sensor.js
@@ -1,1 +1,44 @@
+import { db } from "./firebase-config.js";
+import {
+  ref,
+  onValue
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
+// MENAMPILKAN DATA SENSOR
+export function listenToSensors() {
+  const sensorRef = ref(db, "greenhouse/sensors");
+
+  onValue(sensorRef, (snapshot) => {
+    const d = snapshot.val() || {};
+
+    updateSoil(d.soilMoisture);
+    updateLight(d.lightLevel);
+    updateMotion(d.motion);
+  });
+}
+
+function updateSoil(value) {
+  document.getElementById("soilMoisture").innerText = value + " %";
+  const status =
+    value < 30 ? "Kering" :
+    value < 70 ? "Cukup" :
+    "Basah";
+  document.getElementById("soilStatus").innerText = status;
+}
+
+function updateLight(value) {
+  document.getElementById("lightLevel").innerText = value + " %";
+  const status =
+    value < 30 ? "Redup" :
+    value < 70 ? "Cukup" :
+    "Terang";
+  document.getElementById("lightStatus").innerText = status;
+}
+
+function updateMotion(value) {
+  document.getElementById("motion").innerText =
+    value ? "Terdeteksi" : "Tidak Terdeteksi";
+
+  const status = value ? "Ada Gerakan" : "Aman";
+  document.getElementById("motionStatus").innerText = status;
+}
