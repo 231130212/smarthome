bf3026b26080b5bbb17fbb9b38221ddd668fc9e4
chuseokk <167173348+231130212@users.noreply.github.com>
2025-12-17 21:44:38 +0700

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,29 @@
+import { auth } from "./firebase-config.js";
+import {
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+const dashboard = document.getElementById("dashboard");
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      loginForm.style.display = "none";
+      dashboard.style.display = "block";
+    })
+    .catch(err => {
+      alert("Login gagal: " + err.message);
+    });
+});
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    dashboard.style.display = "none";
+    loginForm.style.display = "block";
+  });
+});
